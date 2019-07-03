6ab6b385c69b014acf64500406a8d224ed722913
问鼎 <postmaster@g000.cn>
2019-07-03 14:10:28 +0800

    配合完成完成新币种的数据库结构设计

--- tests/a.php
+++ tests/a.php
@@ -7,17 +7,17 @@ use GuzzleHttp\Exception\ClientException;
 
 $wallet = new Wallet();
 // $token  = $wallet->token('im@selly.cc', '123456');
-// var_dump($token->data->access_token);
+// var_dump($token);
 
-// $res = $wallet->create('erc20_usdt');
-// var_dump($res);
+$res = $wallet->create('ether');
+var_dump($res);
 
 // $res = $wallet->balance('0x5bf70C262751020496c8634E5aD14994af385ac3');
 // var_dump($res);
 
-try {
-    $res = $wallet->withdraw('0x5bf70C262751020496c8634E5aD14994af385ac3', '0.001', '0x5bf70C262751020496c8634E5aD14994af385ac3', 'erc20_usdt');
-    var_dump($res);
-} catch (ClientException $e) {
-    var_dump($e->getMessage());
-}
+// try {
+//     $res = $wallet->withdraw('0x5bf70C262751020496c8634E5aD14994af385ac3', '0.001', '0x5bf70C262751020496c8634E5aD14994af385ac3', 'erc20_usdt');
+//     var_dump($res);
+// } catch (ClientException $e) {
+//     var_dump($e->getMessage());
+// }
